f90ce79fe068259e9a78334b5287129f2ab0533f
Pralish Kayastha <pralishkayastha@gmail.com>
2025-11-05 21:06:27 -0500

    fluid_cli formula

---
+++ fluid_cli@1.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+#
+require "formula"
+require "fileutils"
+
+class FluidCLIAT1 < Formula
+  module RubyBin
+    def ruby_bin
+      Formula["ruby"].opt_bin
+    end
+  end
+
+  class RubyGemsDownloadStrategy < AbstractDownloadStrategy
+    include RubyBin
+
+    def fetch(_timeout: nil, **_options)
+      ohai("Fetching fluid-cli from gem source")
+      cache.cd do
+        ENV["GEM_SPEC_CACHE"] = "#{cache}/gem_spec_cache"
+
+        _, err, status = Open3.capture3("gem", "fetch", "fluid_cli", "--version", gem_version)
+        unless status.success?
+          odie err
+        end
+      end
+    end
+
+    def cached_location
+      Pathname.new("#{cache}/fluid_cli-#{gem_version}.gem")
+    end
+
+    def cache
+      @cache ||= HOMEBREW_CACHE
+    end
+
+    def gem_version
+      @version ||= @resource&.version if defined?(@resource)
+      raise "Unable to determine version; did Homebrew change?" unless @version
+      @version
+    end
+
+    def clear_cache
+      cached_location.unlink if cached_location.exist?
+    end
+  end
+
+  include RubyBin
+
+  url "fluid_cli", using: RubyGemsDownloadStrategy
+  version "0.1.2"
+  sha256 "007d1230c43c43a28b523bd071f8918079d6f52b35b5ad5919da3a95f2256e7c"
+  depends_on "ruby"
+  depends_on "git"
+
+  def install
+    # set GEM_HOME and GEM_PATH to make sure we package all the dependent gems
+    # together without accidently picking up other gems on the gem path since
+    # they might not be there if, say, we change to a different rvm gemset
+    ENV["GEM_HOME"] = prefix.to_s
+    ENV["GEM_PATH"] = prefix.to_s
+
+    # Use /usr/local/bin at the front of the path instead of Homebrew shims,
+    # which mess with Ruby's own compiler config when building native extensions
+    if defined?(HOMEBREW_SHIMS_PATH)
+      ENV["PATH"] = ENV["PATH"].sub(HOMEBREW_SHIMS_PATH.to_s, "/usr/local/bin")
+    end
+
+    system(
+      "gem",
+      "install",
+      cached_download,
+      "--no-document",
+      "--no-wrapper",
+      "--no-user-install",
+      "--install-dir", prefix,
+      "--bindir", bin,
+      "--",
+      "--skip-cli-build"
+    )
+
+    raise "gem install 'fluid-cli' failed with status #{$CHILD_STATUS.exitstatus}" unless $CHILD_STATUS.success?
+
+    bin.rmtree if bin.exist?
+    bin.mkpath
+
+    brew_gem_prefix = "#{prefix}/gems/fluid_cli-#{version}"
+
+    ruby_libs = Dir.glob("#{prefix}/gems/*/lib")
+    exe = "fluid"
+    file = Pathname.new("#{brew_gem_prefix}/bin/#{exe}")
+    (bin + "#{file.basename}2").open("w") do |f|
+      f << <<~RUBY
+        #!#{ruby_bin}/ruby -rjson --disable-gems
+        ENV['ORIGINAL_ENV']=ENV.to_h.to_json
+        ENV['GEM_HOME']="#{prefix}"
+        ENV['GEM_PATH']="#{prefix}"
+        ENV['RUBY_BINDIR']="#{ruby_bin}/"
+        require 'rubygems'
+        $:.unshift(#{ruby_libs.map(&:inspect).join(",")})
+        load "#{file}"
+      RUBY
+    end
+  end
+end
